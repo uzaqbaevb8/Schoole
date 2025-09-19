e391c1a26e442ea0eeaadcb62c928c143e1bfa47
uzaqbaevb8 <uzaqbaevb8@gmail.com>
2025-09-19 18:42:28 +0500

    dsads

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { MantineProvider } from '@mantine/core';
-import { ModalsProvider } from '@mantine/modals';
-import { Notifications } from '@mantine/notifications';
 createRoot(document.getElementById('root')).render(
     <MantineProvider>
         <ModalsProvider>
